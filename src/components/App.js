238d8ccfe73f6ed4d9f592c088b1e4aff8cbdc8e
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-10 22:32:32 +0300

    learnt useContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import { UserContext } from "./UserContext";
 
 function App() {
   const [theme, setTheme] = useState("dark");
   const [user, setUser] = useState(null);
   return (
     <main className={theme}>
+       <UserContext.Provider value={user}>
       <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
+      <Profile theme={theme} />
+      </UserContext.Provider>
     </main>
   );
 }
